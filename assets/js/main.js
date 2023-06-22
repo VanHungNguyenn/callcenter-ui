7813cc5e34c3549875c17d072cd60566b21d8e82
VanHung <vanhungnguyenbku@gmail.com>
2023-06-22 23:59:34 +0700

    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,15 @@ navbarItems.forEach((item) => {
 		item.classList.add('active')
 	})
 })
+
+const contentDataGroups = document.querySelectorAll('.content__data__group')
+
+contentDataGroups.forEach((group) => {
+	group.addEventListener('click', () => {
+		contentDataGroups.forEach((group) => {
+			group.classList.remove('active')
+		})
+
+		group.classList.add('active')
+	})
+})
