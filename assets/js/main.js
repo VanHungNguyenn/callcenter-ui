9ca5f53576ad3efd93ec70b5802b7991ce0a9eeb
VanHung <vanhungnguyenbku@gmail.com>
2023-06-25 01:09:24 +0700

    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,17 +36,17 @@ jQuery('img.svg').each(function () {
 	)
 })
 
-// const navbarItems = document.querySelectorAll('.navbar__item')
+const navbarItems = document.querySelectorAll('.navbar__item')
 
-// navbarItems.forEach((item) => {
-// 	item.addEventListener('click', () => {
-// 		navbarItems.forEach((item) => {
-// 			item.classList.remove('active')
-// 		})
+navbarItems.forEach((item) => {
+	item.addEventListener('click', () => {
+		navbarItems.forEach((item) => {
+			item.classList.remove('active')
+		})
 
-// 		item.classList.add('active')
-// 	})
-// })
+		item.classList.add('active')
+	})
+})
 
 // const contentDataGroups = document.querySelectorAll('.content__data__group')
 
