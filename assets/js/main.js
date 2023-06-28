320f6a5aa89d052038fc8b1c29a953f950257135
VanHungNguyen <33939159+VanHungNguyenn@users.noreply.github.com>
2023-06-28 17:57:55 +0700

    add id for component

--- assets/js/main.js
+++ assets/js/main.js
@@ -81,43 +81,87 @@ function generatePagination(totalPages, currentPage) {
 
 generatePagination(totalPages, currentPage)
 
-jQuery('img.svg').each(function () {
-	var $img = jQuery(this)
-	var imgID = $img.attr('id')
-	var imgClass = $img.attr('class')
-	var imgURL = $img.attr('src')
+var imgList = document.querySelectorAll('img.svg')
 
-	jQuery.get(
-		imgURL,
-		function (data) {
-			var $svg = jQuery(data).find('svg')
+imgList.forEach(function (img) {
+	var imgID = img.getAttribute('id')
+	var imgClass = img.getAttribute('class')
+	var imgURL = img.getAttribute('src')
+
+	var xhr = new XMLHttpRequest()
+	xhr.open('GET', imgURL, true)
+	xhr.onreadystatechange = function () {
+		if (xhr.readyState === 4 && xhr.status === 200) {
+			var parser = new DOMParser()
+			var svgDoc = parser.parseFromString(
+				xhr.responseText,
+				'image/svg+xml'
+			)
+			var svg = svgDoc.querySelector('svg')
 
 			if (typeof imgID !== 'undefined') {
-				$svg = $svg.attr('id', imgID)
+				svg.setAttribute('id', imgID)
 			}
 
 			if (typeof imgClass !== 'undefined') {
-				$svg = $svg.attr('class', imgClass + ' replaced-svg')
+				svg.setAttribute('class', imgClass + ' replaced-svg')
 			}
 
-			$svg = $svg.removeAttr('xmlns:a')
+			svg.removeAttribute('xmlns:a')
 
 			if (
-				!$svg.attr('viewBox') &&
-				$svg.attr('height') &&
-				$svg.attr('width')
+				!svg.getAttribute('viewBox') &&
+				svg.getAttribute('height') &&
+				svg.getAttribute('width')
 			) {
-				$svg.attr(
+				svg.setAttribute(
 					'viewBox',
-					'0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width')
+					'0 0 ' +
+						svg.getAttribute('width') +
+						' ' +
+						svg.getAttribute('height')
 				)
 			}
 
-			$img.replaceWith($svg)
-		},
-		'xml'
-	)
+			img.parentNode.replaceChild(svg, img)
+		}
+	}
+	xhr.send()
 })
+
+// var imgList = document.getElementsByTagName('img')
+
+// Array.from(imgList).forEach(function (img) {
+// 	if (img.classList.contains('svg')) {
+// 		var imgID = img.getAttribute('id')
+// 		var imgClass = img.getAttribute('class')
+// 		var imgURL = img.getAttribute('src')
+
+// 		var xhr = new XMLHttpRequest()
+// 		xhr.open('GET', imgURL, true)
+// 		xhr.onreadystatechange = function () {
+// 			if (xhr.readyState === 4 && xhr.status === 200) {
+// 				var parser = new DOMParser()
+// 				var svgDoc = parser.parseFromString(
+// 					xhr.responseText,
+// 					'image/svg+xml'
+// 				)
+// 				var svg = svgDoc.querySelector('svg')
+
+// 				if (svg && typeof imgID !== 'undefined') {
+// 					svg.setAttribute('id', imgID)
+// 				}
+
+// 				if (svg && typeof imgClass !== 'undefined') {
+// 					svg.setAttribute('class', imgClass + ' replaced-svg')
+// 				}
+
+// 				img.parentNode.replaceChild(svg, img)
+// 			}
+// 		}
+// 		xhr.send()
+// 	}
+// })
 
 const navbarItems = document.querySelectorAll('.navbar__item')
 
@@ -172,9 +216,9 @@ const scriptDropdown = document.getElementById('script-dropdown')
 
 scriptCheckbox.addEventListener('change', () => {
 	if (scriptCheckbox.checked) {
-		scriptDropdown.classList.add('disabled')
-	} else {
 		scriptDropdown.classList.remove('disabled')
+	} else {
+		scriptDropdown.classList.add('disabled')
 	}
 })
 
